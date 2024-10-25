5f825b6337f2da2d33a58698a02d116a321b7c2c
Rakshit <benjwalrakshit@gmail.com>
2024-10-25 17:06:00 +0530

    Add nim game1

---
+++ NimGame1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<long long> v(n);
+        for(int i=0;i<n;i++)
+            cin>>v[i];
+        long long xo = 0;
+        for(int i=0;i<n;i++){
+            xo = xo^v[i];
+        }
+        if(xo == 0){
+            cout<<"second\n";
+        }else{
+            cout<<"first\n";
+        }
+    }
+}
